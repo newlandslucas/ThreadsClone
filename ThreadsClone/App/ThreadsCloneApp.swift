be13542490284b984988fc3e2e2151a960574348
Lucas Newlands <lucasnewlands@MacBook-Pro-de-Lucas.local>
2023-07-07 12:34:04 -0300

    project setup

---
+++ ThreadsClone/App/ThreadsCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsCloneApp.swift
+//  ThreadsClone
+//
+//  Created by Lucas Newlands on 07/07/23.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
